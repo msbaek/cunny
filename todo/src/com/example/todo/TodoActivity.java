3823ee76d3384367550509801deb81d6f34dde63
Myeongseok Baek <msbaek@daumcorp.com>
2013-01-16 14:45:38 +0900

    setOnKeyListner를 이용해서 OnKeyListener 등록

--- todo/src/com/example/todo/TodoActivity.java
+++ todo/src/com/example/todo/TodoActivity.java
@@ -2,6 +2,8 @@ package com.example.todo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -24,5 +26,19 @@ public class TodoActivity extends Activity {
         final ArrayAdapter<String> aa; // (1) ArrayList의 내용을 ListView와 연동시켜주는 ArrayAdapter를 정의
         aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems); // (2) ArrayAdapter 설정
         listView.setAdapter(aa); // (3) ListView와 ArrayAdapter를 연결
+
+        editText.setOnKeyListener(new View.OnKeyListener() { // (1)
+            public boolean onKey(View v, int keyCode, KeyEvent event) { // (2)
+                if(event.getAction() == KeyEvent.ACTION_DOWN) { // (3)
+                    if(keyCode == KeyEvent.KEYCODE_ENTER) {
+                        todoItems.add(0, editText.getText().toString()); //(4)
+                        aa.notifyDataSetChanged(); // (5)
+                        editText.setText(""); // (6)
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 }
