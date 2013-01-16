9fa4de6a92da9ebf1ff0f2346d4adc7d945b7783
Myeongseok Baek <msbaek@daumcorp.com>
2013-01-16 11:31:46 +0900

    use ImageView in main.xml instead of HelloAndroid.java

--- hello-world/src/com/example/helloworld/HelloAndroid.java
+++ hello-world/src/com/example/helloworld/HelloAndroid.java
@@ -2,7 +2,6 @@ package com.example.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 public class HelloAndroid extends Activity
 {
@@ -11,8 +10,6 @@ public class HelloAndroid extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        ImageView view = new ImageView(this);
-        view.setImageResource(R.drawable.ic_launcher);
-        setContentView(view);
+        setContentView(R.layout.main);
     }
 }
