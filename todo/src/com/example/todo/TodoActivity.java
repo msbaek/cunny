4517f51119f3fbfa26a8d7ddac8076c8f5b482db
Myeongseok Baek <msbaek@daumcorp.com>
2013-01-16 13:48:25 +0900

    GUI 객체(myListView)와 코드의 객체(listView)를 연결
    findViewById 메소드를 이용해서 layout에 정의된 객체를 얻고 이를 코드 상의 변수에 할당

--- todo/src/com/example/todo/TodoActivity.java
+++ todo/src/com/example/todo/TodoActivity.java
@@ -2,6 +2,8 @@ package com.example.todo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class TodoActivity extends Activity {
     /**
@@ -11,5 +13,8 @@ public class TodoActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ListView listView = (ListView) findViewById(R.id.myListView); // GUI 객체(myListView)와 코드의 객체(listView)를 연결
+        final EditText editText = (EditText) findViewById(R.id.myEditTExt);
     }
 }
